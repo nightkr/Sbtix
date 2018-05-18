b1c4abc9b51abbbed1b1f4b8d64fa54c95842f38
Gabriel Gonzalez <gabriel@awakenetworks.com>
2018-05-18 12:00:41 -0700

    Pin `nixpkgs` and `sbt`
    I accidentally installed Sbtix using a recent version of `nixpkgs` where
    `sbt` defaults to version 1.1.4 so this change pins `sbt` to `0.13.13` instead
    and also pins `nixpkgs` to the latest stable release (18.03) to improve the
    determinism of the installer

--- default.nix
+++ default.nix
@@ -1,2 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  fetchNixpkgs = import ./fetchNixpkgs.nix;
+
+  nixpkgs = fetchNixpkgs {
+     rev          = "78d4a1e3ea294ef5f1dcba110825831c02f3a46c";
+     sha256       = "0sv1f6bwbisijp1h0g3qvv43m5hq74xlwsnv5wm27mbi8lzgm057";
+     outputSha256 = "1f4x4bkssclmcprd0lm52sjlnnyfmabgxff8cdf68y6dc1lkhz5z";
+  };
+
+  pinSBT = packagesNew: packagesOld: {
+    sbt = packagesNew.callPackage ./sbt.nix { };
+  };
+
+in
+
+{ pkgs ? import nixpkgs { overlays = [ pinSBT ]; } }:
 pkgs.callPackage ./sbtix-tool.nix {}
